43031dbb13606d9d3f5962af51d0d32d5e4aab9b
Colby Williams <ColbyLWilliams@gmail.com>
2019-10-15 10:36:48 -0400

    Create x2go-mate.sh

---
+++ x2go-mate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+apt update && \
+apt install ubuntu-mate-desktop && \
+apt-get install software-properties-common && \
+add-apt-repository ppa:x2go/stable && \
+apt-get update && \
+apt-get install x2goserver x2goserver-xsession && \
+apt-get install x2gomatebindings
