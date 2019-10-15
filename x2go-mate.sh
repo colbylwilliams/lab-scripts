7f4b819f2cd9ac321d671212924f5a2ec1788ef6
Colby Williams <ColbyLWilliams@gmail.com>
2019-10-15 16:00:51 -0400

    Update x2go-mate.sh

--- x2go-mate.sh
+++ x2go-mate.sh
@@ -1,8 +1,68 @@
 #!/bin/sh
 
-apt install ubuntu-mate-desktop && \
-apt-get install software-properties-common && \
-add-apt-repository ppa:x2go/stable && \
-apt-get update && \
-apt-get install x2goserver x2goserver-xsession && \
-apt-get install x2gomatebindings
+set -e
+
+error() {
+    echo ${RED}"Error: $@"${RESET} >&2
+}
+
+setup_color() {
+    # Only use colors if connected to a terminal
+    if [ -t 1 ]; then
+        RED=$(printf '\033[31m')
+        GREEN=$(printf '\033[32m')
+        YELLOW=$(printf '\033[33m')
+        BLUE=$(printf '\033[34m')
+        BOLD=$(printf '\033[1m')
+        RESET=$(printf '\033[m')
+    else
+        RED=""
+        GREEN=""
+        YELLOW=""
+        BLUE=""
+        BOLD=""
+        RESET=""
+    fi
+}
+
+
+main() {
+
+    setup_color
+
+    
+    echo "${BLUE}Adding x2go PPA repo...${RESET}"
+
+    apt-get install software-properties-common
+
+    add-apt-repository ppa:x2go/stable
+
+
+    echo "${BLUE}Updating apt package repository...${RESET}"
+
+    apt-get update
+
+    sleep 2
+
+    
+    echo "${BLUE}Intalling MATE desktop and x2go server...${RESET}"
+
+    apt-get install 
+
+    
+    echo "${BLUE}Intalling x2go server...${RESET}"
+
+    apt-get install ubuntu-mate-desktop x2goserver x2goserver-xsession x2gomatebindings
+
+
+    echo "${GREEN}MATE desktop and x2go installed successfully!${RESET}"    
+}
+
+main
+
+# apt install ubuntu-mate-desktop && \
+# apt-get install software-properties-common && \
+# add-apt-repository ppa:x2go/stable && \
+# apt-get update && \
+# apt-get install x2goserver x2goserver-xsession && \
+# apt-get install x2gomatebindings
